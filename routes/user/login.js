afac6f308b04607d3aaff9d0b54f18121e07d7e3
MEGHAMAHESHJOSHI <22meghajoshi@gmail.com>
2020-10-15 10:17:48 +0530

    passport.js

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,45 +1,88 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
+
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
 const regRouter = express.Router();
 
-regRouter.post(
-  "/",
-  [
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters",
-    ).isLength({ min: 6 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+// regRouter.post(
+//   "/",
+//   [
+//     check("email", "Please include a valid email").isEmail(),
+//     check(
+//       "password",
+//       "Please enter a password with 6 or more characters",
+//     ).isLength({ min: 6 }),
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { email, password } = req.body;
+
+//     try {
+//       let user = await User.findOne({ email });
+//       if (!user) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: "Email not registered" }] });
+//       }
+//       const isMatch = await bcrypt.compare(password, user.password);
+
+//       if (!isMatch) {
+//         return res
+//           .status(400)
+//           .json({ errors: [{ msg: "Invalid Credentials" }] });
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//       res.status(500).send("Server serror");
+//     }
+//   },
+// );
+
+
+
+// regRouter.post('/',async (req, res, next) => {
+//     passport.authenticate('login',async (err, user, info) => {
+//         try {
+//           if (err || !user) {
+//             const error = new Error('An error occurred.');
+
+//             return next(error);
+//           }
+
+//           req.login(
+//             user,
+//             { session: false },
+//             async (error) => {
+//               if (error) return next(error);
+
+//               const body = { _id: user._id, email: user.email };
+//               const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+//               return res.json({ token });
+//             }
+//           );
+//         } catch (error) {
+//           return next(error);
+//         }
+//       }
+//     )(req, res, next);
+//   }
+// );
+
+regRouter.post('/', passport.authenticate('jwt', { session: false }),
+    function(req, res) {
+      res.send('done');
+        res.send(req.user.profile);
     }
-
-    const { email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-      if (!user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Email not registered" }] });
-      }
-      const isMatch = await bcrypt.compare(password, user.password);
-
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server serror");
-    }
-  },
 );
+
+
+module.exports = router;
 
 module.exports = regRouter;
